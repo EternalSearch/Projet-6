559c423d8e5697071d09e9951cccf5a6d155ba60
Florence <loison.florence@gmail.com>
2024-07-20 14:56:29 +0200

    Page d'accueil terminée, ajout page de connexion

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,50 @@
+
+const categories = await fetch('http://localhost:5678/api/categories');
+const categoriesResponse = await categories.json();
+
+function filterCategories(categoriesResponse) {
+
+    for (let i = 0; i < categoriesResponse.length; i++) {
+        if (i === 0) {
+            const filterContainer = document.querySelector(".portfoliobuttons_filter");
+            const filterButtonTous = document.createElement("div");
+            filterButtonTous.classList.add("filterbutton")
+
+            filterButtonTous.innerText = "Tous";
+
+            filterContainer.appendChild(filterButtonTous);
+        }
+
+        const category = categoriesResponse[i]
+        const filterContainer = document.querySelector(".portfoliobuttons_filter");
+        const filterButton = document.createElement("div");
+        filterButton.classList.add("filterbutton")
+        filterButton.innerText = category.name;
+
+        filterContainer.appendChild(filterButton);
+
+    }
+}
+filterCategories(categoriesResponse);
+
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+const filterBtnList = document.querySelectorAll(".filterbutton");
+for (let i = 0; i < filterBtnList.length; i++) {
+    filterBtnList[i].addEventListener("click", () => {
+        const filteredWorks = worksResponse.filter(function (work) {
+            if (i === 0) {
+                return work.categoryId;
+            } else {
+                return work.categoryId === i;
+            }
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        displayWorks(filteredWorks);
+    });
+}
+
+
 // Récupération depuis l'Api et conversion des travaux en Json
 const works = await fetch("http://localhost:5678/api/works");
 const worksResponse = await works.json();
@@ -5,7 +52,7 @@ const worksResponse = await works.json();
 // Création fonction d'affichage travaux
 function displayWorks(worksResponse) {
     for (let i = 0; i < worksResponse.length; i++) {
-        const work = worksResponse[i]
+        const work = worksResponse[i];
         const worksContainer = document.querySelector(".gallery");
         const figureElement = document.createElement("figure");
         const imgElement = document.createElement("img");
@@ -20,6 +67,19 @@ function displayWorks(worksResponse) {
         worksContainer.appendChild(figureElement);
         figureElement.appendChild(imgElement);
         figureElement.appendChild(figcaptionElement);
+
     }
 }
-displayWorks(worksResponse)
+displayWorks(worksResponse);
+ cxc xxxxx
+
+// import {displayListeneremail}from "./avis.js";
+// displayListeneremail()
+
+// filter.addEventListener("click",function
+// (categoriesResponse){;
+
+// });
+
+
+
