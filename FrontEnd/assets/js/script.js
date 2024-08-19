ad143360ab9c7280ee95bc43cbe8a6d66c69818e
Florence <loison.florence@gmail.com>
2024-08-19 20:27:21 +0200

    Intégration page login

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,3 +1,36 @@
+
+const categories = await fetch('http://localhost:5678/api/categories');
+const categoriesResponse = await categories.json();
+
+const filterCategories = (categoriesResponse) => {
+    // Affichage de la barre de filtres
+    const filterContainer = document.querySelector(".portfolio__filterbar");
+    var structure = "<div class='portfolio__filterbutton'>Tous</div>"
+    filterContainer.innerHTML = structure
+    categoriesResponse.forEach(category => {
+        structure = `<div class="portfolio__filterbutton">${category.name}</div>`
+        filterContainer.innerHTML += structure
+    })
+    // Ajout des listeners
+    const filterBtnList = document.querySelectorAll(".portfolio__filterbutton");
+    for (let i = 0; i < filterBtnList.length; i++) {
+        filterBtnList[i].addEventListener("click", () => {
+            const filteredWorks = worksResponse.filter(function (work) {
+                if (i === 0) {
+                    return work.categoryId;
+                } else {
+                    return work.categoryId === i;
+                }
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            console.log(filteredWorks);
+
+            displayWorks(filteredWorks);
+        });
+    }
+}
+filterCategories(categoriesResponse);
+
 // Récupération depuis l'Api et conversion des travaux en Json
 const works = await fetch("http://localhost:5678/api/works");
 const worksResponse = await works.json();
